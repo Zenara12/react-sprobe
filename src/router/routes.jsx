d8894e65f050d8bced6dd16ea1e17fbc9daba61c
Zenara12 <nolipernes@gmail.com>
2025-03-28 02:51:33 +0800

    Add: router and routes

---
+++ src/router/routes.jsx
@@ -0,0 +1,54 @@
+import DefaultLayout from '../layout/DefaultLayout';
+import GuestLayout from '../layout/GuestLayout';
+import Login from '../pages/Login';
+import NotFound404 from '../pages/NotFound404';
+import Signup from '../pages/Signup';
+import Users from '../pages/Users';
+import Dashboard from '../pages/Dashboard';
+import { Navigate } from 'react-router-dom';
+
+const routes = [
+	{
+		path: '/',
+		element: <DefaultLayout />,
+		children: [
+			{
+				path: '/',
+				element: <Navigate to='/dashboard' />,
+			},
+			{
+				path: '/dashboard',
+				element: <Dashboard />,
+			},
+			{
+				path: '/users',
+				element: <Users />,
+			},
+		],
+	},
+	{
+		path: '/',
+		element: <GuestLayout />,
+		children: [
+			{
+				path: '/',
+				element: <Navigate to='/login' />,
+			},
+			{
+				path: '/login',
+				element: <Login />,
+			},
+			{
+				path: '/signup',
+				element: <Signup />,
+			},
+		],
+	},
+
+	{
+		path: '*',
+		element: <NotFound404 />,
+	},
+];
+
+export default routes;
