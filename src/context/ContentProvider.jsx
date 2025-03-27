457afb966d3d46fa301a27586ec8912502bbb78d
Zenara12 <nolipernes@gmail.com>
2025-03-28 02:46:10 +0800

    Add:ContextProvider for token state

---
+++ src/context/ContentProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState, useContext } from 'react';
+
+const StateContext = createContext({
+	user: { name: '' },
+	token: '',
+	setUser: () => {},
+	setToken: () => {},
+});
+
+export const ContextProvider = ({ children }) => {
+	const [user, setUser] = useState({ name: '' });
+	const [token, _setToken] = useState(localStorage.getItem('token') || '');
+
+	const setToken = (token) => {
+		_setToken(token);
+		if (token) {
+			localStorage.setItem('token', String(token));
+		}
+	};
+
+	return (
+		<StateContext.Provider value={{ user, token, setUser, setToken }}>
+			{children}
+		</StateContext.Provider>
+	);
+};
+
+export const useStateContext = () => useContext(StateContext);
