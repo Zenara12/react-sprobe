fa0179640b5e015c3deab33060e7cba569eaab01
Zenara12 <nolipernes@gmail.com>
2025-03-29 16:11:05 +0800

    update: card text responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,17 @@
-function Card({ item }) {
+function Card({ item, children }) {
 	return (
 		<>
 			<div className='sm:mx-auto sm:w-full sm:min-w-sm md:max-w-sm bg-white p-6 rounded-lg shadow-md flex sm:items-center md:items-start'>
 				<div>
-					<h4 className='text-lg font-semibold'>{item.title}</h4>
-					<p className='text-gray-500'>{item.body}</p>
+					<h4 className='text-lg font-semibold break-all line-clamp-2'>
+						{item.title}
+					</h4>
+					<p className='text-gray-500 break-all line-clamp-2'>{item.body}</p>
 					<p className='text-slate-700 mt-2 text-sm'>
 						{new Date(item.created_at).toLocaleTimeString()}
 					</p>
 				</div>
+				<div className='size-6 flex justify-end w-full mt-auto'>{children}</div>
 			</div>
 		</>
 	);
