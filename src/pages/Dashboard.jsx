4d27012581eaa39cbe5ffd3c4871214338b1bb5d
Zenara12 <nolipernes@gmail.com>
2025-03-30 15:37:54 +0800

    update task count number

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,33 @@
-
+import { useEffect, useState } from 'react';
+import axiosClient from '../axios';
+import Card from '../components/Card';
 
 export default function Dashboard() {
-  return (
-    <>
-		<main>
-				<div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">content</div>
-		</main>
-    </>
-  )
+	const [tasksCount, setTasksCount] = useState(0);
+
+	function getTasks() {
+		axiosClient
+			.get('/api/tasks')
+			.then(({ data }) => {
+				setTasksCount(data.length);
+			})
+			.catch((err) => {
+				const response = err.response;
+				if (response && response.status === 422) {
+					console.error(response);
+				}
+			});
+	}
+
+	useEffect(() => {
+		getTasks();
+	}, [tasksCount]);
+
+	return (
+		<>
+			<div className='mt-10 mx-2 md:mx-auto'>
+				<Card title={'Tasks'} body={`#${tasksCount}`} />
+			</div>
+		</>
+	);
 }
